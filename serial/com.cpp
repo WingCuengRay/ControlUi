b8121671306a9c275b55f45ea7eb91f89509ec47
Raymand <yongconglei@gmail.com>
2016-09-30 07:06:27 +0800

    Add RIO package (Debug Mode)

--- serial/com.cpp
+++ serial/com.cpp
@@ -5,33 +5,38 @@ bool Com::OpenCom()
 	int ret = open(name.c_str(), O_RDWR|O_NOCTTY);
 	if(ret == -1)
 		return false;
-	fd = ret;
+	//rio_pack.rio_fd = ret;
+	rio_readinitb(&rio_pack, ret);
 	
 	return true;
 }
 
 bool Com::bind()
 {
-	if(fd == -1)
+	if(rio_pack.rio_fd == -1)
 		return false;
-	return port->setConfiguration(fd);
+	return port->setConfiguration(rio_pack.rio_fd);
 }
 
-int Com::recv_data(char *buf, size_t len)
+int Com::recv_data(unsigned char *buf, size_t len)
 {
-	int ret = read(fd, buf, len);
+	//int ret = read(fd, buf, len);
+	int ret = rio_readnb(&rio_pack, buf, len);
 
 	return ret;
 }
 
+using namespace std;
 int Com::recv_OneByte(unsigned char *buf)
 {
-	int ret = read(fd, buf, 1);
+	int ret = rio_readnb(&rio_pack, buf, 1);
 	if(ret == 1 && *buf==0x7d)
 	{
-		ret = read(fd, buf, 1);
-		*buf ^= 0x20;		//遇到转义字符 '7d' 将其后面第一个字符的第六位取反
+		ret = rio_readnb(&rio_pack, buf, 1);
+		if(ret == 1)
+			*buf ^= 0x20;		//遇到转义字符 '7d' 将其后面第一个字符的第六位取反
 	}
+	//cout << "hex: " << hex << (int)(*buf) << endl; 
 	
 	return ret;
 }
--- serial/com.h
+++ serial/com.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "SerialPort.h"
 #include <memory>
+#include "rio.h"
 
 extern "C"
 {
@@ -21,22 +22,30 @@ public:
 	
 	Com(const std::string &nme, std::shared_ptr<Port> p) : name(nme), port(p)
 	{
-		fd = -1;
+		//fd = -1;
+		
+		rio_pack.rio_fd = -1;
+		rio_pack.rio_cnt = 0;
+		rio_pack.rio_bufptr = rio_pack.rio_buf;
 	}
 	Com(std::string nme) : name(nme)
 	{
 		//这句代码增加了 COM 和 SerialPort 的耦合性
 		//port->setPortAttr(9600, 0, EIGHT, ONE, N);
-		fd = -1;
+		//fd = -1;
+		
+		rio_pack.rio_fd = -1;
 	}
 	
 	bool OpenCom();
 	bool bind();
-	int recv_data(char *buf, size_t len);
+	int recv_data(unsigned char *buf, size_t len);
 	int recv_OneByte(unsigned char *buf);
 	
 private:
 	int fd;
+	
+	rio_t rio_pack;
 	std::shared_ptr<Port> port;
 };
 
